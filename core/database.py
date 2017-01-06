70e11a8f2383757fce60fb81d9d6896a6496ff8f
alexlep <alex.lep.sp@gmail.com>
2017-01-06 23:28:49 +0100

    Small import change in DB file

--- core/database.py
+++ core/database.py
@@ -2,16 +2,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.pool import QueuePool, StaticPool
 
 # SQLite, for tests
 
+from sqlalchemy.pool import StaticPool
 url = 'sqlite:///ppalette.sqlite'
 engine = create_engine(url,
                     connect_args={'check_same_thread':False},
                     poolclass=StaticPool)
 # MySQL
 '''
+from sqlalchemy.pool import QueuePool
 url = 'mysql://test:test@localhost/palette?use_unicode=1&charset=utf8'
 engine = create_engine(url, pool_recycle=3600, pool_size=20, max_overflow=0,
                        poolclass=QueuePool,
@@ -20,11 +21,10 @@ engine = create_engine(url, pool_recycle=3600, pool_size=20, max_overflow=0,
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=True,
                                          bind=engine))
-
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-def init_db(create_tables = False):
+def init_db(create_tables=False):
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
