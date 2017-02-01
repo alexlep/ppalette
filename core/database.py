da55643977d99a05b58e4fd77901febab4a7b426
alexlep <alex.lep.sp@gmail.com>
2017-02-01 20:17:15 +0100

    DB Connection
    Now TCP sockect is used to connect to Mysql,
    instead if unix socket

--- core/database.py
+++ core/database.py
@@ -13,9 +13,14 @@ engine = create_engine(url,
 # MySQL
 '''
 from sqlalchemy.pool import QueuePool
-url = 'mysql://test:test@localhost/palette?use_unicode=1&charset=utf8'
-engine = create_engine(url, pool_recycle=3600, pool_size=20, max_overflow=0,
-                       poolclass=QueuePool,
+
+mysql_host = '127.0.0.1'
+mysql_port = 3306
+url = 'mysql://test:test@{}/palette?use_unicode=1&charset=utf8'.\
+      format(mysql_host)
+engine = create_engine(url, poolclass=QueuePool,
+                       connect_args=dict(host=mysql_host, port=mysql_port),
+                       pool_recycle=3600, pool_size=20, max_overflow=0,
                        isolation_level="READ UNCOMMITTED")
 '''
 db_session = scoped_session(sessionmaker(autocommit=False,
