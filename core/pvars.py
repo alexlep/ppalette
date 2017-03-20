80a8288170d1694b027dbc5e04e81fdf6b2c3504
alexlep <alex.lep.sp@gmail.com>
2017-03-20 19:56:29 +0100

    Add new configuration file and variables file

---
+++ core/pvars.py
@@ -0,0 +1,31 @@
+import os
+
+#workingDir = os.path.dirname(os.path.abspath(__file__))
+workingDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+configDir = workingDir + '/config'
+redConfigFile = configDir + '/red_config.json'
+greyConfigFile = configDir + '/grey_config.json'
+violetConfigFile = configDir + '/violet_config.json'
+commonConfigFile = configDir + '/common.json'
+#
+rrdDataDir = workingDir + '/rrd_data'
+statRRDFile = rrdDataDir + '/common_statistics.rrd'
+
+#
+prefix = '/redapi'
+HOST = prefix + '/host'
+HOSTS = prefix + '/hosts'
+PLUGIN = prefix + '/plugin'
+PLUGINS = prefix + '/plugins'
+SUITE = prefix + '/suite'
+SUITES = prefix + '/suites'
+SUBNET = prefix + '/subnet'
+SUBNETS = prefix + '/subnets'
+STATUS = prefix + '/status'
+DISCOVERY = prefix + '/discovery'
+SCHEDULER = prefix + '/scheduler'
+WORKERS = prefix + '/workers'
+MONITORING = prefix + '/monitoring'
+
+#
+defTimeFormat = "%H:%M:%S:%d:%m:%Y"
