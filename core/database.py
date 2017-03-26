c5eba3a3f7794129b1e884392781c1f82fae04dc
alexlep <alex.lep.sp@gmail.com>
2017-03-26 11:02:02 +0200

    corrected database name

--- core/database.py
+++ core/database.py
@@ -16,8 +16,11 @@ from sqlalchemy.pool import QueuePool
 
 mysql_host = '127.0.0.1'
 mysql_port = 3306
-url = 'mysql://test:test@{}/palette?use_unicode=1&charset=utf8'.\
-      format(mysql_host)
+dbuser = 'test'
+dbpassword = 'test'
+
+url = 'mysql://{0}:{1}@{2}/ppalette?use_unicode=1&charset=utf8'.\
+      format(dbuser, dbpassword, mysql_host)
 engine = create_engine(url, poolclass=QueuePool,
                        connect_args=dict(host=mysql_host, port=mysql_port),
                        pool_recycle=3600, pool_size=20, max_overflow=0,
